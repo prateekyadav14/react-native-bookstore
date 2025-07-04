c33da309d24418bf89348e95d426e304cdb6409a
Prateek Yadav <prateekyadav14@gmail.com>
2025-07-04 17:51:46 +0530

    added createdAt

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -56,6 +56,7 @@ router.post("/register", async (req, res) => {
                 username: user.username,
                 email: user.email,
                 profileImage: user.profileImage,
+                createdAt: user.createdAt,
             },
         });
 
@@ -95,6 +96,7 @@ router.post("/login", async (req, res) => {
                 username: user.username,
                 email: user.email,
                 profileImage: user.profileImage,
+                createdAt: user.createdAt,
             },
         });
 
